7450662d2ed8b6776c81097b2c9122a00f53bf08
Haden Fletcher <hello@hadenfletcher.com>
2019-02-02 04:07:11 -0600

    basic runner

--- run.js
+++ run.js
@@ -2,10 +2,13 @@
 
 const args = process.argv.slice(2);
 
-if (args.length < 1) {
+if (!process.env["NODE_ENV"] && args.length < 1) {
     console.error("Please specify environment");
-    process.exit(1);
+    return process.exit(1);
+} else if (args.length > 0) {
+    process.env["NODE_ENV"] = args[0].toLowerCase();
+    return process.exit(require("./bin/bot").run());
 } else {
-    process.env["NODE_ENV"] = args[0];
-    require("./bin/bot").run();
+    process.env["NODE_ENV"] = "production";
+    return process.exit(require("./bin/bot").run());
 }
