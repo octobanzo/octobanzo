1b6b03a855c00b4fbff91275f486c97f99b7324f
Haden Fletcher <haden@hadenfletcher.com>
2019-02-06 21:11:58 -0600

    updated runner!

--- run.js
+++ run.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2);
+(() => {
+    const args = process.argv.slice(2);
+    const run = require("./bin/base").run;
 
-if (!process.env["NODE_ENV"] && args.length < 1) {
-    console.error("Please specify environment");
-    return process.exit(1);
-} else if (args.length > 0) {
-    process.env["NODE_ENV"] = args[0].toLowerCase();
-    require("./bin/base").run();
-} else {
-    process.env["NODE_ENV"] = "production";
-    require("./bin/base").run();
-}
+    if (!process.env["NODE_ENV"] && args.length === 0) {
+        console.error("ERR: Please specify node environment! Set NODE_ENV environment variable or supply as first argument.");
+        return process.exit(1);
+    } else if (args.length > 0) {
+        process.env["NODE_ENV"] = args[0].toLowerCase();
+        return run();
+    } else {
+        process.env["NODE_ENV"] = "production";
+        return run();
+    }
+})(); // Please keep wrapped. Exposed JS is a crime!
