8e257160ccd806beb6d050524e34237be80c7bc9
Haden Fletcher <haden@hadenfletcher.com>
2019-02-15 15:32:46 -0600

    turn language processing into a module instead of core function

---
+++ src/mod/language.ts
@@ -0,0 +1,97 @@
+import * as config from "config";
+import { Message } from "discord.js";
+import { MessageResponse, Wit } from "node-wit";
+import Bot from "../lib/bot";
+import { Module } from "../lib/modules";
+
+export default class Language extends Module {
+    private app: Bot;
+    private wit: Wit;
+
+    constructor(app: Bot) {
+        super({
+            name: "LanguageProcessor",
+            requiredSettings: "nlp.enable",
+            version: "0.1.0-dev",
+        });
+
+        this.app = app;
+        this.wit = new Wit({ accessToken: config.get("nlp.wit_token") });
+
+        // register event handlers
+        this.handle("message", this.handleMessage);
+    }
+
+    public static accuracy(input: number): number {
+        return Math.round(input * 1000) / 10;
+    }
+
+    public static analysis(response: MessageResponse): string {
+        const data = response.entities;
+        let reply = ``;
+
+        // What was the intent?
+        if (data.intent) {
+            reply += `Intent: **${data.intent[0].value}** (${Language.accuracy(data.intent[0].confidence)}%)\n`;
+        }
+
+        // Did it target someone?
+        if (data.contact) {
+            reply += `Target: **${data.contact[0].value}** (${Language.accuracy(data.contact[0].confidence)}%)\n`;
+        }
+
+        // Detection of sentiment
+        if (data.sentiment) {
+            reply += `Sentiment: **${data.sentiment[0].value}** (${Language.accuracy(data.sentiment[0].confidence)}%)\n`;
+        }
+
+        // Is it a hello?
+        if (data.greeting) {
+            reply += `Greeting: **yes** (${Language.accuracy(data.greeting[0].confidence)}%)\n`;
+        }
+
+        // Does it mention the bot?
+        if (data.mentions_bot) {
+            reply += `Mentions bot: **yes** (${Language.accuracy(data.mentions_bot[0].confidence)}%)\n`;
+        }
+
+        // If there's no data, soulja boy tell 'em
+        if (reply.length === 0) {
+            reply += `No data.`;
+        }
+
+        return reply;
+    }
+
+    public async understand(input: string): Promise<MessageResponse> {
+        return this.wit.message(input, {});
+    }
+
+    private async handleMessage(msg: Message): Promise<void> {
+        console.log(msg.content);
+        // ignore all bot and ignore-char-starting messages
+        if (msg.author.bot
+            || msg.content.startsWith(config.get("nlp.ignore_prefix") || null)) {
+            return;
+        }
+
+        // if in NLP-restricted mode, only analyze messages in test channel
+        if (config.get("nlp.restrict")
+            && msg.channel.id !== config.get("nlp.test_channel")) {
+            return;
+        }
+
+        // now let's analyze the message
+        const understanding = await this.understand(msg.content.replace(/[\*\_\|\`\~]+/gi, ""));
+
+        // send raw response to master logs, if any
+        if (this.app.nlpLogChannel) {
+            this.app.nlpLogChannel.send(`\`\`\`json\n${JSON.stringify(understanding, null, 2)}\`\`\``);
+        }
+
+        // if it's in test channel, show response
+        if (msg.channel.id === (config.get("nlp.test_channel") || null)) {
+            msg.channel.send(Language.analysis(understanding));
+        }
+    }
+}
