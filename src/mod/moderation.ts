eda1bfcac2c394931da5af2ab6216e23d29b1d3e
Haden Fletcher <haden@hadenfletcher.com>
2019-02-15 15:32:59 -0600

    skeleton for moderation module

---
+++ src/mod/moderation.ts
@@ -0,0 +1,13 @@
+import { Module } from "../lib/modules";
+
+export default class Moderation extends Module {
+    constructor() {
+        super({
+            description: "Assist admins in managing servers.",
+            name: "Moderation",
+            version: "0.0.1-dev",
+        });
+
+        // TODO
+    }
+}
