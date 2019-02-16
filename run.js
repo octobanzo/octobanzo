cf66f72fbe5e13a82aa9c1b9f6c08b3617597770
Haden Fletcher <haden@hadenfletcher.com>
2019-02-16 00:14:41 -0600

    add config for source map support

--- run.js
+++ run.js
@@ -6,17 +6,19 @@ const debug = require("debug")("runner:initial");
 
 (() => {
     debug("App started.");
+    require("source-map-support").install({
+        emptyCacheBetweenOperations: true,
+        environment: "node"
+    });
+    debug("Enabled source map support for stack traces.");
 
-    require("source-map-support").install();
-    debug("Enabled sourcemap support.");
-
-    if (args.length > 0) { // override NODE_ENV
+    if (args.length > 0) { // override NODE_ENV if passed as argument
         process.env.NODE_ENV = args[0].toLowerCase();
     } else if (!process.env.NODE_ENV) {
-        console.error("ERR: Please specify node environment! Set NODE_ENV environment variable or supply as first argument.");
+        console.error("ERROR: Please specify node environment! Set NODE_ENV environment variable or supply as first argument.");
         return process.exit(1);
     }
 
-    debug("Initialization completed; running");
+    debug("Initialization completed. Running");
     return run();
 })(); // Please keep wrapped. Exposed JS is a crime!
