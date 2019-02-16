e1000793f9fd6b9fc1d6b5d2d9c631d40eb3806e
Haden Fletcher <haden@hadenfletcher.com>
2019-02-16 12:21:54 -0600

    [runner] only enable source map logging when in development

--- run.js
+++ run.js
@@ -6,17 +6,20 @@ const debug = require("debug")("runner:initial");
 
 (() => {
     debug("App started.");
-    require("source-map-support").install({
-        emptyCacheBetweenOperations: true,
-        environment: "node"
-    });
-    debug("Enabled source map support for stack traces.");
 
     if (args.length > 0) { // override NODE_ENV if passed as argument
         process.env.NODE_ENV = args[0].toLowerCase();
     } else if (!process.env.NODE_ENV) {
         console.error("ERROR: Please specify node environment! Set NODE_ENV environment variable or supply as first argument.");
         return process.exit(1);
+    }
+
+    if (process.env.NODE_ENV === "development") {
+        require("source-map-support").install({
+            emptyCacheBetweenOperations: true,
+            environment: "node"
+        });
+        debug("Enabled source map support for stack traces.");
     }
 
     debug("Initialization completed. Running");
