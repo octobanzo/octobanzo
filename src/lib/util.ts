e39ad943383fd36cff345e3c8e2fb49e3370509a
Haden Fletcher <haden@hadenfletcher.com>
2019-02-17 23:44:08 -0600

    make delay static

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -11,7 +11,7 @@ export default class Utilities {
      * Wait an amount of time.
      * @param time Time in milliseconds to wait.
      */
-    public async delay(time: number): Promise<void> {
+    public static async delay(time: number): Promise<void> {
         return new Promise((resolve) => setTimeout(resolve, time));
     }
 }
