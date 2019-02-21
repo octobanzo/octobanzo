5a3d9e1cd17ba9c0c44f5634a76490f7bea0075e
Haden Fletcher <haden@hadenfletcher.com>
2019-02-21 01:45:56 -0600

    Ban logging!

--- src/mod/moderation.ts
+++ src/mod/moderation.ts
@@ -1,17 +1,113 @@
-import { Message } from "discord.js";
+import { get as conf } from "config";
+import { Guild, GuildAuditLogs, GuildAuditLogsEntry, Message, RichEmbed, TextChannel, User } from "discord.js";
 import Bot from "../lib/bot";
+import Logger from "../lib/logging";
 import { Module } from "../lib/modules";
+import { default as Utils } from "../lib/util";
+
+const debug = Logger.debugLogger("module:moderation");
 
 export default class Moderation extends Module {
+    private app: Bot;
+
     constructor(app: Bot) {
         super({
-            description: "Assists server admins with moderation via machine learning.",
-            name: "Moderation",
+            description: "Assists server admins with moderation.",
             version: "0.0.1-dev",
         });
 
-        this.handle("message", (msg: Message) => {
-            // TODO: Moderation message handler
+        this.app = app;
+
+        this.handle("message", this.handleMessage);
+        this.handle("guildBanAdd", this.handleBan);
+    }
+
+    private async handleMessage(msg: Message) {
+        // TODO: Use LanguageProcessor to detect bad, give an overall score
+    }
+
+    private async handleBan(guild: Guild, user: User) {
+        if (!conf("moderation.logs.events.ban")
+            || !conf("moderation.logs.channel")) { return; }
+
+        const timestamp = new Date();
+
+        if (conf("moderation.auto_unban.enable")) {
+            if ((conf("moderation.auto_unban.user_id") as string[]).includes(user.id)
+                && guild.id === conf("moderation.auto_unban.guild_id")) {
+                try {
+                    guild.unban(user, "Automatic unban for debugging. (bot debug)");
+                    this.app.log.debug("Unbanned test user.", { user: user.tag, guild: guild.name });
+                } catch (err) {
+                    this.app.log.error(err, "Could not unban test user.");
+                }
+            }
+        }
+
+        await Utils.delay(250); // Delay .25s before doing anything. (Allows audit log to update, it's slow af for some reason)
+
+        let results: GuildAuditLogs;
+
+        try {
+            results = await guild.fetchAuditLogs({ type: "MEMBER_BAN_ADD", limit: 1 });
+        } catch (err) {
+            this.app.log.error(err, "Could not fetch audit log entry!");
+            return;
+        }
+
+        const logChannel: TextChannel = guild.channels.get(conf("moderation.logs.channel")) as TextChannel;
+        const result: GuildAuditLogsEntry = results.entries.first();
+        let reason: string = result.reason;
+
+        if (result.executor.id === this.app.user.id) { return; }
+
+        if (!result.executor.bot && !reason) {
+            try {
+                const responseChannel = await (result.executor.dmChannel || await result.executor.createDM());
+                const awaitTime = 1000 * (conf("moderation.logs.reason_time") as number);
+                await responseChannel.send(`Please tell me your reason for banning ${user.tag}. I'll wait ${Utils.formatTime(Utils.convertTime(awaitTime))} for your response.`);
+                const response = await responseChannel.awaitMessages((msg: Message) => (msg.author.id !== this.app.user.id),
+                    { max: 1, time: awaitTime });
+                if (response.size > 0) {
+                    response.map((msg) => { reason = msg.content; });
+                    responseChannel.send(`Thanks, I'll include that in the logs.`);
+                } else {
+                    responseChannel.send(`I didn't get a reason from you. None will be logged.`);
+                    // TODO: Conditionally, tell person to ask admin to add reason or add themselves if admin
+                }
+            } catch (err) {
+                debug(`Error getting ban reason. See log.`);
+                this.app.log.error(err, "Error fetching ban reason.");
+            }
+        }
+
+        const logAttachment = new RichEmbed({
+            description: "User Banned",
+            color: 0xFF3333,
+            fields: [{
+                name: "User",
+                value: `\`${user.tag.replace("`", "'")}\``,
+                inline: true
+            }, {
+                name: "Banned by",
+                value: result.executor.tag,
+                inline: true
+            }, {
+                name: "Reason",
+                value: reason || "<no reason provided>",
+                inline: false
+            }],
+            timestamp
         });
+
+        try {
+            debug("Sending ban log...");
+            await logChannel.send(logAttachment);
+            debug("Ban log sent!");
+            return;
+        } catch (err) {
+            this.app.log.warn(err, "Couldn't send ban log.");
+            return;
+        }
     }
 }
