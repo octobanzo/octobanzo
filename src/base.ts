3288e02c89fea5d62773daedcfed8cc7b4a1077d
Haden Fletcher <haden@hadenfletcher.com>
2019-02-22 23:58:30 -0600

    merge runner and base

--- src/base.ts
+++ src/base.ts
@@ -1,16 +1,37 @@
 import { final } from "pino";
 import Bot from "./lib/bot";
 import Logger from "./lib/logging";
+import { debug as debugLib } from "debug";
 
+const args = process.argv.slice(2);
 const debug = Logger.debugLogger("runner:base");
 
-export function run(): void {
+function start(): void {
+    debug("App started.");
+
+    // override NODE_ENV if passed as argument
+    if (args.length > 0) {
+        process.env.NODE_ENV = args[0].toLowerCase();
+    } else if (!process.env.NODE_ENV) {
+        console.error("ERROR: Please specify environment! Set NODE_ENV or supply environment as first argument.");
+        process.exit(1);
+    }
+
+    // Add source map logging if 'development'
+    if (process.env.NODE_ENV === "development") {
+        require("source-map-support").install({
+            environment: "node"
+        });
+        debug("Enabled source map support for stack traces.");
+    }
+
     const app = new Bot();
 
     process.on("uncaughtException", app.log.error);
     process.on("unhandledRejection", app.log.warn);
     process.on("SIGINT", () => shutdown(app));
 
+    debug("Initialization completed. Running");
     return;
 }
 
@@ -25,3 +46,5 @@ function shutdown(app: Bot): never {
         return process.exit(1);
     }
 }
+
+start();
