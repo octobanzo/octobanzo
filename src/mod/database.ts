7dec16663838f1eed6d2ba584b56dd7a84f61b42
Haden Fletcher <haden@hadenfletcher.com>
2019-02-28 02:33:23 -0600

    database skeleton

--- src/mod/database.ts
+++ src/mod/database.ts
@@ -3,14 +3,23 @@ import { Message } from "discord.js";
 import Bot from "../lib/bot";
 import Logger from "../lib/logging";
 import { Module } from "../lib/modules";
+import * as knex from "knex";
 
 export default class Database extends Module {
+    public lib: knex;
+
     private app: Bot;
+    private tables;
 
     constructor(app: Bot) {
         super({
             requiredSettings: "database",
             version: "0.0.1",
+        });
+
+        this.lib = knex({
+            client: conf("database.client"),
+            connection: conf("database.connection")
         });
 
         this.app = app;
