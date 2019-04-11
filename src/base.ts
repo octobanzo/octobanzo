d533096dd3a87285f75900d5ddb1a8a4735e53a4
Haden Fletcher <haden@hadenfletcher.com>
2019-04-11 00:19:10 -0500

    remove some basel logging

--- src/base.ts
+++ src/base.ts
@@ -1,23 +1,20 @@
-import Bot from "./lib/bot";
+import Bot from './lib/bot';
 
 export function start(): void {
-    console.info("INIT.BASE: Starting bot...");
-
     const app = new Bot();
 
-    process.on("uncaughtException", console.error);
-    process.on("unhandledRejection", console.error);
-    process.on("SIGINT", () => shutdown(app));
-
-    console.log("INIT: Initial");
+    process.on('uncaughtException', console.error);
+    process.on('unhandledRejection', console.error);
+    process.on('SIGINT', () => shutdown(app));
 }
 
 function shutdown(app: Bot): never {
-    app.log.info("Shutting down...");
+    app.log.info('Shutting down...');
+
     try {
-        app.log.debug("Destroying client");
+        app.log.debug('Destroying client');
         app.client.destroy();
-        app.log.debug("Client destroyed.");
+        app.log.debug('Client destroyed.');
         return process.exit(0);
     } catch (err) {
         return process.exit(1);
