d683f1b6a27c6b81d1a7a92ce29c991dc400479c
Haden Fletcher <haden@hadenfletcher.com>
2019-04-11 00:19:40 -0500

    code style

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,10 +1,10 @@
-import { get as conf } from "config";
-import * as Discord from "discord.js";
-import * as pino from "pino";
-import { default as modulesList } from "../mod/_index";
-import Commands from "../mod/commands";
-import Logger from "./logging";
-import { ModuleManager } from "./modules";
+import { get as conf } from 'config';
+import * as Discord from 'discord.js';
+import * as pino from 'pino';
+import { default as modulesList } from '../mod/_index';
+import Commands from '../mod/commands';
+import Logger from './logging';
+import { ModuleManager } from './modules';
 
 export default class Bot {
     public client: Discord.Client;
@@ -21,8 +21,8 @@ export default class Bot {
         this.client = new Discord.Client({
             disableEveryone: true,
             disabledEvents: [
-                "PRESENCE_UPDATE",
-                "TYPING_START",
+                'PRESENCE_UPDATE',
+                'TYPING_START',
             ],
         });
 
@@ -30,9 +30,9 @@ export default class Bot {
     }
 
     private async init(): Promise<void> {
-        this.log.info("Bot started.");
+        this.log.info('Bot started.');
 
-        this.client.on("ready", async () => {
+        this.client.on('ready', async () => {
             this.user = this.client.user;
             this.log.info(`Discord connected as ${this.user.tag}, id:${this.user.id}`);
 
@@ -41,31 +41,31 @@ export default class Bot {
             this.modules.postInit();
         });
 
-        this.client.on("error", async (err: Error) => {
+        this.client.on('error', async (err: Error) => {
             this.log.error(err, `Client error!`);
         });
 
         try {
-            this.log.debug("Registering modules...");
+            this.log.debug('Registering modules...');
             for (const mod of modulesList) {
                 this.log.trace(`Registering module ${mod.name}`);
-                const module = new mod(this);
-                this.modules.add(module);
+                const instance = new mod(this);
+                this.modules.add(instance);
 
-                if (mod.name === "Commands") { this.commands = module as Commands; }
+                if (mod.name === 'Commands') { this.commands = instance as Commands; }
             }
             this.modules.init(this.client);
-            this.log.info("Modules registered.");
+            this.log.info('Modules registered.');
         } catch (err) {
-            this.log.error(err, "Error setting up modules.");
+            this.log.error(err, 'Error setting up modules.');
             return process.exit(1);
         }
 
         try {
-            this.log.debug("Connecting to Discord...");
-            await this.client.login(conf("discord.token"));
+            this.log.debug('Connecting to Discord...');
+            await this.client.login(conf('discord.token'));
         } catch (err) {
-            this.log.error(err, "Could not log into Discord!");
+            this.log.error(err, 'Could not log into Discord!');
             return process.exit(1);
         }
 
