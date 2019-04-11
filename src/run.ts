3256571a2707013b4c57a169c5c6da38d59f0612
Haden Fletcher <haden@hadenfletcher.com>
2019-04-11 11:03:03 -0500

    runner format: inherent run rather than special expression

--- src/run.ts
+++ src/run.ts
@@ -1,26 +1,26 @@
-/* tslint:disable no-require-imports */
+/* tslint:disable:no-require-imports */
 
 const args = process.argv.slice(2);
 
-function run(): void {
-    // override NODE_ENV if passed as argument
-    (args.length) &&
-        (process.env.NODE_ENV = args[0].toLowerCase());
+(
+    function run(): void {
+        // override NODE_ENV if passed as argument
+        (args.length) &&
+            (process.env.NODE_ENV = args[0].toLowerCase());
 
-    if (!process.env.NODE_ENV) {
-        console.error('[ERR!] Please specify environment! Set NODE_ENV or supply environment as first argument.');
-        process.exit(1);
+        if (!process.env.NODE_ENV) {
+            console.error('[ERR!] Please specify environment! Set NODE_ENV or supply environment as first argument.');
+            process.exit(1);
+        }
+
+        console.info('[INIT] Environment: ' + process.env.NODE_ENV);
+
+        // Add source map logging if 'development'
+        (process.env.NODE_ENV === 'development') &&
+            require('source-map-support').install({
+                environment: 'node'
+            });
+
+        require('./base').start();
     }
-
-    console.info('[INIT] Environment OK. ' + process.env.NODE_ENV);
-
-    // Add source map logging if 'development'
-    (process.env.NODE_ENV === 'development') &&
-        require('source-map-support').install({
-            environment: 'node'
-        });
-
-    require('./base').start();
-}
-
-run();
+)()
