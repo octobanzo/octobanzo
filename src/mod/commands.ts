c7c75960e6b76b46ece8c28fce3d28b5ac24b128
Haden Fletcher <haden@hadenfletcher.com>
2019-04-14 16:24:14 -0500

    [commands] add cmd permissions and types

--- src/mod/commands.ts
+++ src/mod/commands.ts
@@ -1,5 +1,5 @@
 import { get as conf } from 'config';
-import { Message } from 'discord.js';
+import { Guild, GuildMember, Message, RichEmbed, UserResolvable } from 'discord.js';
 import Bot from '../lib/bot';
 import Logger from '../lib/logging';
 import { Module } from '../lib/modules';
@@ -30,6 +30,59 @@ export default class Commands extends Module {
         return this;
     }
 
+    public async canUse(user: UserResolvable, command: ICommandOptions | string): Promise<boolean> {
+        // if label is provided, get command object
+        if (typeof command === 'string') {
+            command = this.labels[command];
+        }
+
+        // Resolve user
+        if (user instanceof Message) {
+            if (user.member) {
+                user = user.member;
+            } else {
+                user = user.author;
+            }
+        } else if (user instanceof Guild) {
+            user = user.owner;
+        } else if (typeof user === 'string') {
+            user = await this.app.client.fetchUser(user);
+        }
+
+        // check if command type is guild, then if user is guild member
+        if (command.type === 'guild'
+            && !(user instanceof GuildMember)) {
+            return false;
+        }
+
+        if (command.permission === CommandPermission.User) {
+            return true;
+        }
+
+        if (command.permission === CommandPermission.AppOwner
+            && user.id === this.app.owner.id) {
+            return true;
+        }
+
+        // if user is in a guild and permissions require role checks
+        if (user instanceof GuildMember) {
+            if (command.permission === CommandPermission.Moderator) {
+                return (/* statement checking if user has role */ false);
+            }
+
+            if (command.permission === CommandPermission.Administrator) {
+                return (/* statement checking if user has role */ false);
+            }
+
+            if (command.permission === CommandPermission.GuildOwner
+                && user.id === user.guild.id) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private async handleMessage(msg: Message): Promise<void> {
         if (msg.author.bot) {
             return;
@@ -44,8 +97,36 @@ export default class Commands extends Module {
 
         this.app.log.trace('Potential command!');
         if (Object.keys(this.labels || {}).includes(label)) {
-            this.app.log.trace('Command found!');
-            this.commands[this.labels[label].name](this.labels[label], msg, label, args);
+            this.app.log.trace(`Executing command: ${label}`);
+            const cmd = this.labels[label];
+            try {
+                if (!this.canUse(msg.author, cmd)) throw new Error('User cannot use this command');
+                await this.commands[cmd.name](cmd, msg, label, args);
+            } catch (err) {
+                this.app.log.debug(err, 'Command failure');
+                msg.channel.send(new RichEmbed({
+                    color: 0xFF0000,
+                    title: ':warning: **An error occurred!**',
+                    description: `I couldn't execute that command.`,
+                    fields: [{
+                        name: 'Command',
+                        value: `${prefix}${label} ${args.join(' ')}`,
+                        inline: true
+                    }, {
+                        name: 'Executor',
+                        value: `${msg.author}`,
+                        inline: true
+                    }, {
+                        name: 'Error message',
+                        value: err.message || err.msg || err,
+                        inline: false
+                    }],
+                    footer: {
+                        text: `Contact the bot owner (${this.app.owner.username}) for help if you think this is a bug.`
+                    }
+                }))
+                    .catch((err) => this.app.log.trace(err, 'Could not send error message.'));
+            }
         }
 
         return;
@@ -55,5 +136,18 @@ export default class Commands extends Module {
 export interface ICommandOptions {
     name: string;
     description?: string;
+    type: CommandType;
+    permission: CommandPermission;
     aliases?: string[];
+    usage?: string;
+}
+
+export type CommandType = 'guild' | 'user' | 'open';
+
+export enum CommandPermission {
+    User,
+    Moderator,
+    Administrator,
+    GuildOwner,
+    AppOwner
 }
