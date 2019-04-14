2ae105cafd5b6bfa636fe5c1ff9fef1424bee7a0
Haden Fletcher <haden@hadenfletcher.com>
2019-04-14 16:25:52 -0500

    add skeleton for help command

--- src/mod/utils.ts
+++ src/mod/utils.ts
@@ -2,6 +2,7 @@ import { get as conf } from 'config';
 import Bot from '../lib/bot';
 import Logger from '../lib/logging';
 import { Module } from '../lib/modules';
+import { CommandPermission } from './commands';
 
 export default class Utils extends Module {
     private app: Bot;
@@ -17,8 +18,25 @@ export default class Utils extends Module {
         app.commands.add({
             name: 'eval',
             aliases: ['evaluate', 'expression'],
-            description: 'Evaluate a JavaScript expression.'
+            description: 'Evaluate a JavaScript expression.',
+            usage: '<expression>',
+            type: 'open',
+            permission: CommandPermission.AppOwner
         }, this.evalCommand.bind(this));
+
+        app.commands.add({
+            name: 'help',
+            aliases: ['?'],
+            description: 'Get a list of commands and help with how to use them.',
+            usage: '[command]',
+            type: 'open',
+            permission: CommandPermission.User
+        }, this.helpCommand.bind(this));
+    }
+
+    private async helpCommand(cmd, msg, label, args): Promise<void> {
+        // skeleton. add logic later
+        return;
     }
 
     private async evalCommand(cmd, msg, label, args): Promise<void> {
