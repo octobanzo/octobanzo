55eb7ffca8af7ba901c5d8cfc2d7de48f65d45eb
Haden <haden@hadenfletcher.com>
2019-07-07 20:57:15 -0500

    new chunkArray utility

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,11 @@
-import Bot from './bot';
+import Bot from './bot'
 
 export default class Utilities {
-    private app: Bot;
+    private app: Bot
 
     constructor(app?: Bot) {
         // set this.app only if app supplied
-        app && (this.app = app);
+        app && (this.app = app)
     }
 
     /**
@@ -13,7 +13,7 @@ export default class Utilities {
      * @param time Time in milliseconds to wait.
      */
     public static async delay(time: number): Promise<void> {
-        return new Promise((resolve) => setTimeout(resolve, time));
+        return new Promise((resolve) => setTimeout(resolve, time))
     }
 
     /**
@@ -21,30 +21,40 @@ export default class Utilities {
      * @param time Time to convert, in milliseconds.
      */
     public static convertTime(time: number): { days, hours, minutes, seconds } {
-        let seconds = Math.floor(time / 1000);
-        let minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
-        let hours = Math.floor(minutes / 60);
-        minutes = minutes % 60;
-        const days = Math.floor(hours / 24);
-        hours = hours % 24;
+        let seconds = Math.floor(time / 1000)
+        let minutes = Math.floor(seconds / 60)
+        seconds = seconds % 60
+        let hours = Math.floor(minutes / 60)
+        minutes = minutes % 60
+        const days = Math.floor(hours / 24)
+        hours = hours % 24
         return {
             days,
             hours,
             minutes,
             seconds
-        };
+        }
     }
 
     public static formatTime(time: { days, hours, minutes, seconds }): string {
-        const response = [];
+        const response = []
 
-        if (time.days) { response.push(`${time.days} days`); }
-        if (time.hours) { response.push(`${time.hours} hours`); }
-        if (time.minutes) { response.push(`${time.minutes} minutes`); }
-        if (time.seconds) { response.push(`${time.seconds} seconds`); }
+        if (time.days) { response.push(`${time.days} days`) }
+        if (time.hours) { response.push(`${time.hours} hours`) }
+        if (time.minutes) { response.push(`${time.minutes} minutes`) }
+        if (time.seconds) { response.push(`${time.seconds} seconds`) }
 
         // Join together with commas, adding "and" before last one
-        return response.join(', ').replace(/, ([^,]*)$/, ', and $1');
+        return response.join(', ').replace(/, ([^,]*)$/, ', and $1')
+    }
+
+    public static chunkArray(array, size): any[][] {
+        const chunkedArr = []
+        let index = 0
+        while (index < array.length) {
+            chunkedArr.push(array.slice(index, size + index))
+            index += size
+        }
+        return chunkedArr
     }
 }
