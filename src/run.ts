893c6ef174339504a8714ff05c0e9ca8a2d105b9
Haden Fletcher <haden@hadenfletcher.com>
2020-06-08 04:34:35 -0500

    restructure run file (might completely redo later)
    Signed-off-by: Haden Fletcher <haden@hadenfletcher.com>

--- src/run.ts
+++ src/run.ts
@@ -1,38 +1,40 @@
+/* eslint-disable no-console */
+
 require('dotenv/config')
 
 import { install as installSourceMap } from 'source-map-support'
 import Bot from './lib/bot'
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
-(
-	function run(): void {
-		// override NODE_ENV if passed as argument
-		(args.length) &&
-			(process.env.NODE_ENV = args[0].toLowerCase())
+function run(): void {
+	// override NODE_ENV if passed as argument
+	(args.length) &&
+		(process.env.NODE_ENV = args[0].toLowerCase())
 
-		if (!process.env.NODE_ENV) {
-			console.error('[ERR!] Please specify environment! Set NODE_ENV or supply environment as first argument.')
-			process.exit(1)
-		}
-
-		console.info('[INIT] Environment: ' + process.env.NODE_ENV);
-
-		// Add source map logging if 'development'
-		(process.env.NODE_ENV === 'development') &&
-			installSourceMap({
-				environment: 'node'
-			})
-
-		const app = new Bot()
-
-		process.on('uncaughtException', console.error)
-		process.on('unhandledRejection', console.error)
-		process.on('SIGINT', () => shutdown(app))
+	if (!process.env.NODE_ENV) {
+		console.error('[ERR!] Please specify environment! Set NODE_ENV or supply environment as first argument.')
+		process.exit(1)
 	}
-)()
 
-function shutdown(app: Bot): Promise<never> {
+	console.info('[INIT] Environment: ' + process.env.NODE_ENV)
+
+	// Add source map logging if 'development'
+	if (process.env.NODE_ENV === 'development')
+		installSourceMap({
+			environment: 'node'
+		})
+
+	const app = new Bot()
+
+	process.on('uncaughtException', console.error)
+	process.on('unhandledRejection', console.error)
+	process.on('SIGINT', () => shutdown(app))
+	process.on('SIGKILL', () => shutdown(app))
+	process.on('SIGTERM', () => shutdown(app))
+}
+
+function shutdown(app: Bot): never {
 	app.log.info('Shutting down')
 
 	try {
@@ -44,3 +46,5 @@ function shutdown(app: Bot): Promise<never> {
 		return process.exit(1)
 	}
 }
+
+run()
