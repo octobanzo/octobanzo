a0ef6dd497c8d8e4bf2a4078e52c6b5b02a9cdee
Haden Fletcher <33473181+hadenpf@users.noreply.github.com>
2020-09-03 21:53:01 -0500

    improve plain-js runner

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+#! /usr/bin/env node
+
 const path = require('path');
 const fs = require('fs');
+const package = require('./package.json');
 
 console.log('Starting Octobanzo...');
 
 fs.exists(path.join(__dirname, 'bin', 'index.js'), (exists) => {
-    if (!exists)
-        return console.error('Run file not found. Have you run "yarn build" yet?');
+    if (!exists) {
+        let repoUrl = package?.repository?.url;
+        if (repoUrl.endsWith('.git')) repoUrl = repoUrl.slice(0, -4);
+
+        return console.error(
+            'Run file not found. Please run "yarn build".\n',
+            'If running "yarn build" does not work for you,\n',
+            'please create an issue on GitHub:\n',
+            `${repoUrl ? repoUrl + '/issues' : '[missing GitHub URL]'}`
+        );
+    }
 
     const { run } = require('./bin/index');
     run();
